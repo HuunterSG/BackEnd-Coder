42ad5804e7e56dcddb2163e024faaa6901242f92
HuunterSG <agustinjperez.r@gmail.com>
2022-07-21 13:34:03 -0300

    back ok db ok, Falta dar estilos esteticos en front y lograr q se vean las img

--- desafio clase15-16/src/services/dataSets.js
+++ desafio clase15-16/src/services/dataSets.js
@@ -1,5 +1,15 @@
 const productos = [
+    {
+        title: 'Fernet Branca',
+        price: 1450,
+        thumbnail:'https://d3ugyf2ht6aenh.cloudfront.net/stores/001/182/795/products/agregar-un-titulo-201-e6d1f9523fae150fa316348193930014-1024-1024.png'
+    },
+    {
+        title:'Fernet Branca x12',
+        price: 12000,
+        thumbnail:'https://d3ugyf2ht6aenh.cloudfront.net/stores/001/182/795/products/agregar-un-titulo-201-e6d1f9523fae150fa316348193930014-1024-1024.png'
 
+    }
 ];
 
 const mensajes = [
